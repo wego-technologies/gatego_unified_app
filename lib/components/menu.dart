47e02f0bfb29a39ab2bc602cd144feaab98218de
Aguilaair <eduardomoreno.eduardo@gmail.com>
2021-06-10 18:09:38 +0200

    Fix material splash

--- lib/components/menu.dart
+++ lib/components/menu.dart
@@ -29,155 +29,159 @@ class Menu extends StatelessWidget {
         color: Theme.of(context).canvasColor,
       ),
       padding: const EdgeInsets.symmetric(horizontal: 5),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          leading ?? const SizedBox(),
-          SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            child: Column(
-              children: !expanded
-                  ? menuItems.entries.map((item) {
-                      bool _itemSelcted = item.key == selectedItemKey;
-                      return Container(
-                        margin: EdgeInsets.symmetric(vertical: 5),
-                        child: IconButton(
-                          onPressed: () {
-                            onItemPressed(item.key);
-                          },
-                          icon: Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: _itemSelcted
-                                  ? Theme.of(context).primaryColor
-                                  : Colors.transparent,
-                              shape: BoxShape.circle,
+      child: Material(
+        color: Colors.transparent,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            leading ?? const SizedBox(),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: Column(
+                children: !expanded
+                    ? menuItems.entries.map((item) {
+                        bool _itemSelcted = item.key == selectedItemKey;
+                        return Container(
+                          margin: EdgeInsets.symmetric(vertical: 5),
+                          child: IconButton(
+                            onPressed: () {
+                              onItemPressed(item.key);
+                            },
+                            icon: Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: _itemSelcted
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.transparent,
+                                shape: BoxShape.circle,
+                              ),
+                              child: HeroIcon(
+                                item.value,
+                                size: 30,
+                                color: _itemSelcted
+                                    ? Theme.of(context).canvasColor
+                                    : Theme.of(context).iconTheme.color,
+                              ),
                             ),
-                            child: HeroIcon(
-                              item.value,
-                              size: 30,
-                              color: _itemSelcted
-                                  ? Theme.of(context).canvasColor
-                                  : Theme.of(context).iconTheme.color,
-                            ),
+                            iconSize: 50,
+                            splashRadius: 30,
                           ),
-                          iconSize: 50,
-                          splashRadius: 30,
-                        ),
-                      );
-                    }).toList()
-                  : menuItems.entries.map((item) {
-                      bool _itemSelcted = item.key == selectedItemKey;
-                      return Container(
-                        constraints:
-                            const BoxConstraints(minWidth: double.infinity),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 13, horizontal: 8),
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(5000),
-                          onTap: () {
-                            onItemPressed(item.key);
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(10),
-                            //margin: EdgeInsets.all(10),
-                            width: 170,
-                            decoration: BoxDecoration(
-                              color: _itemSelcted
-                                  ? Theme.of(context).primaryColor
-                                  : Colors.transparent,
-                              borderRadius: BorderRadius.circular(5000),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                HeroIcon(
-                                  item.value,
-                                  size: 30,
-                                  color: _itemSelcted
-                                      ? Theme.of(context).canvasColor
-                                      : Theme.of(context).iconTheme.color,
-                                ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Text(
-                                  item.key,
-                                  textAlign: TextAlign.end,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 15,
+                        );
+                      }).toList()
+                    : menuItems.entries.map((item) {
+                        bool _itemSelcted = item.key == selectedItemKey;
+                        return Container(
+                          constraints:
+                              const BoxConstraints(minWidth: double.infinity),
+                          margin:
+                              EdgeInsets.symmetric(vertical: 13, horizontal: 8),
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(5000),
+                            onTap: () {
+                              onItemPressed(item.key);
+                            },
+                            child: Container(
+                              padding: EdgeInsets.all(10),
+                              //margin: EdgeInsets.all(10),
+                              width: 170,
+                              decoration: BoxDecoration(
+                                color: _itemSelcted
+                                    ? Theme.of(context).primaryColor
+                                    : Colors.transparent,
+                                borderRadius: BorderRadius.circular(5000),
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  HeroIcon(
+                                    item.value,
+                                    size: 30,
                                     color: _itemSelcted
-                                        ? Colors.white
+                                        ? Theme.of(context).canvasColor
                                         : Theme.of(context).iconTheme.color,
                                   ),
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                              ],
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    item.key,
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 15,
+                                      color: _itemSelcted
+                                          ? Colors.white
+                                          : Theme.of(context).iconTheme.color,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    }).toList(),
+                        );
+                      }).toList(),
+              ),
             ),
-          ),
-          trailing ?? SizedBox(),
-          Container(
-            //constraints: const BoxConstraints(minWidth: double.infinity),
-            margin: EdgeInsets.symmetric(vertical: 13, horizontal: 8),
-            child: InkWell(
-              borderRadius: BorderRadius.circular(5000),
-              onTap: () {
-                onExpandedToggle();
-              },
-              child: Container(
-                padding: EdgeInsets.all(10),
-                //margin: EdgeInsets.all(10),
-                //width: 170,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5000),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    HeroIcon(
-                      !expanded
-                          ? HeroIcons.chevronRight
-                          : HeroIcons.chevronLeft,
-                      size: 30,
-                      color: Theme.of(context).iconTheme.color,
-                    ),
-                    if (expanded)
-                      SizedBox(
-                        width: 10,
+            trailing ?? SizedBox(),
+            Container(
+              //constraints: const BoxConstraints(minWidth: double.infinity),
+              margin: EdgeInsets.symmetric(vertical: 13, horizontal: 8),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(5000),
+                onTap: () {
+                  onExpandedToggle();
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  //margin: EdgeInsets.all(10),
+                  //width: 170,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5000),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HeroIcon(
+                        !expanded
+                            ? HeroIcons.chevronRight
+                            : HeroIcons.chevronLeft,
+                        size: 30,
+                        color: Theme.of(context).iconTheme.color,
                       ),
-                    if (expanded)
-                      Text(
-                        "Contract Menu",
-                        textAlign: TextAlign.end,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 15,
-                          color: Theme.of(context).iconTheme.color,
+                      if (expanded)
+                        SizedBox(
+                          width: 10,
                         ),
-                      ),
-                    if (expanded)
-                      SizedBox(
-                        width: 5,
-                      ),
-                  ],
+                      if (expanded)
+                        Text(
+                          "Minimize Menu",
+                          textAlign: TextAlign.end,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                            color: Theme.of(context).iconTheme.color,
+                          ),
+                        ),
+                      if (expanded)
+                        SizedBox(
+                          width: 5,
+                        ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gatego_unified_app/components/menu.dart';
 import 'package:heroicons/heroicons.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(AppWrapper());
@@ -14,6 +15,9 @@ class AppWrapper extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Color(0xFF00ADE2),
+        textTheme: GoogleFonts.robotoTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       home: MenuWrapper(),
     );
