774836bcfdf0f5645c563f0e383489b4419dba20
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-07-08 20:27:40 +0200

    Create comingSoon.dart

---
+++ lib/pages/comingSoon.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+
+class ComingSoonPage extends StatelessWidget {
+  const ComingSoonPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          HeroIcon(
+            HeroIcons.lightningBolt,
+            solid: true,
+            color: Colors.yellow.shade800,
+            size: 100,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Coming Soon",
+            style: Theme.of(context).textTheme.headline3,
+          ),
+        ],
+      ),
+    );
+  }
+}
